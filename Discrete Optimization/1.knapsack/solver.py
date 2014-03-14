8c089f8b2a527f3adbb2a1b9884f98eb2ee8f5eb
Ren Cheng-Bin <renchengbin.billy@gmail.com>
2014-03-14 18:21:07 +0800

    增加  根据价值密度排序的算法
    待解决：
    1. 明显不可行的选择是否要先过滤掉（单个重量大于K）；
    2. 不知道排序是否高效。在10000个物品下应该做一个单机测试。

--- Discrete Optimization/1.knapsack/solver.py
+++ Discrete Optimization/1.knapsack/solver.py
@@ -15,14 +15,15 @@ def solve_it(input_data):
     capacity = int(firstLine[1])
 
     items = []
-
+    '''
     for i in range(1, item_count+1):
         line = lines[i]
         parts = line.split()
         items.append(Item(i-1, int(parts[0]), int(parts[1])))
-
+    '''
     # a trivial greedy algorithm for filling the knapsack
     # it takes items in-order until the knapsack is full
+    '''
     value = 0
     weight = 0
     taken = [0]*len(items)
@@ -32,7 +33,20 @@ def solve_it(input_data):
             taken[item.index] = 1
             value += item.value
             weight += item.weight
+    '''
+
+    #@TODO：尝试按照价值密度来写一个算法
+    for i in range(1, item_count+1):
+        line = lines[i]
+        parts = line.split()
+        items.append(Item(i-1, int(parts[0]), int(parts[1]), int(parts[0] / parts[1])))
+
+    #sort using value / weight
+    value = 0
+    weight = 0
+    taken = [0] * len(items)
     
+
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(0) + '\n'
     output_data += ' '.join(map(str, taken))
