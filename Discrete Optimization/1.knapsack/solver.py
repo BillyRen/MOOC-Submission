fbe288c077788b6f46e4711a94e335d91304b2b5
Ren Cheng-Bin <renchengbin.billy@gmail.com>
2014-03-20 10:07:36 +0800

    增加  动态规划算法

--- Discrete Optimization/1.knapsack/solver.py
+++ Discrete Optimization/1.knapsack/solver.py
@@ -4,6 +4,25 @@
 from collections import namedtuple
 Item = namedtuple("Item", ['index', 'value', 'weight'])
 
+def knapsack_dynamic(w, p, n, m, x):
+    #计算optp[i][j]
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            optp[i][j] = optp[i - 1][j]
+            if (j >= w[i]) and (optp[i - 1][j - w[i]] + p[i] > optp[i - 1][j]):
+                optp[i][j] = optp[i - 1][j - w[i]] + p[i]
+
+    #递推装入背包的物体
+    j = m
+    for i in range(n, 0, -1):
+        if optp[i][j] > optp[i - 1][j]:
+            x[i-1] = True
+            j = j - w[i]
+
+    #返回最大价值
+    v = optp[n][m]
+    return v, x
+
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
 
@@ -15,26 +34,29 @@ def solve_it(input_data):
     capacity = int(firstLine[1])
 
     items = []
-    '''
+
     for i in range(1, item_count+1):
         line = lines[i]
         parts = line.split()
         items.append(Item(i-1, int(parts[0]), int(parts[1])))
-    '''
+
     # a trivial greedy algorithm for filling the knapsack
     # it takes items in-order until the knapsack is full
-    '''
+
     value = 0
     weight = 0
     taken = [0]*len(items)
+
+    #TODO: Transfer key into 2 lists
+    value, taken = knapsack_dynamic(weight, value, item_count, capacity, taken)
 
     for item in items:
         if weight + item.weight <= capacity:
             taken[item.index] = 1
             value += item.value
             weight += item.weight
-    '''
 
+    '''
     #@TODO：尝试按照价值密度来写一个算法
     for i in range(1, item_count+1):
         line = lines[i]
@@ -45,7 +67,7 @@ def solve_it(input_data):
     value = 0
     weight = 0
     taken = [0] * len(items)
-    
+    '''
 
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(0) + '\n'
